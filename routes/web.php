62797f1d9a9aec235af2dc1d9528960aba07bcf1
Rangga Adithia <rangga.adithia26@gmail.com>
2023-09-16 11:39:00 +0800

    make a test routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WelcomeController::class, 'index']);
 Route::get('/home', [WelcomeController::class, 'home']);
+Route::get("/test", function () {
+ return "hello";
+});
